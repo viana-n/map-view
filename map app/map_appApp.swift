b8cec9e0ec80afca8b115629d47a3e0419594ddf
Viana Najafi <viananajafi@icloud.com>
2023-06-29 09:55:56 -0700

    Initial Commit

---
+++ map app/map_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  map_appApp.swift
+//  map app
+//
+//  Created by Viana Najafi on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct map_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
